6c2534631d532555b7e226613c52137b2ad1658c
brandaolarissa-cmd <brandao.larissa@escola.pr.gov.br>
2026-09-23 11:07:43 +0000

    alteraçao

---
+++ script.js
@@ -0,0 +1,20 @@
+function mostraralerta(){
+
+}
+
+functin pedirnome(){
+   let nome = prompt ("informe o seu nome ")
+ alert ('bom dia,' + nome 
+ }
+
+ function somarnumeros(){
+    let primeirovalor = Number (promp("infomre o primeiro numero: "))
+    let segundovalor = Number(prompt("informe o segundo numero"))
+    let soma = primeirovalor + segundovalor 
+
+    alert("a soma dos numeros é: " + soma)
+ }
+
+ function mudarformataçao(){
+    let resultado 
+ }
